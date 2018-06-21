023e1e85082bf697e25e74cfceb6ff5cddb60276
Wayne Treible <wayne.treible@visionsystemsinc.com>
2018-06-21 15:53:54 -0400

    Adding (local) rational camera & geocam definitions to pyvpgl

--- pyvpgl.cxx
+++ pyvpgl.cxx
@@ -2,6 +2,7 @@
 #include <vpgl/vpgl_proj_camera.h>
 #include <vpgl/vpgl_affine_camera.h>
 #include <vpgl/vpgl_perspective_camera.h>
+#include <vpgl/vpgl_rational_camera.h>
 #include <vpgl/vpgl_lvcs.h>
 #include <vpgl/vpgl_utm.h>
 #include <vgl/vgl_point_3d.h>
@@ -11,6 +12,18 @@
 #include <vgl/vgl_vector_2d.h>
 #include <vgl/vgl_homg_point_2d.h>
 #include <vgl/vgl_homg_point_3d.h>
+
+#include <vpgl/file_formats/vpgl_geo_camera.h>
+#include <vil/vil_load.h>
+#include <vil/vil_image_resource.h>
+#include <vil/vil_image_resource_sptr.h>
+
+// For rational to affine //
+#include <vnl/vnl_random.h>
+#include <vpgl/vpgl_local_rational_camera.h>
+#include <vpgl/algo/vpgl_affine_rectification.h>
+// ====================== //
+
 #include "pyvxl_util.h"
 
 #include <pybind11/pybind11.h>
@@ -64,7 +77,100 @@ void wrap_vpgl(py::module &m)
     .def(py::init<vpgl_calibration_matrix<double>, vgl_rotation_3d<double>, vgl_vector_3d<double> >())
     .def("__str__", stream2str<vpgl_perspective_camera<double> >);
 
+  // Geo- Camera definitions
+  py::class_<vpgl_geo_camera>(m, "geo_camera")
+    // Default methods
+    .def(py::init<>())
+    .def("__str__", stream2str<vpgl_geo_camera >)
+    // Convert pixel coords (u,v) to a lon/lat pair
+    .def("img_to_global",
+      [](vpgl_geo_camera &G, double const u, double const v)
+      {
+        double lon, lat;
+        G.img_to_global(u, v, lon, lat);
+        return std::make_tuple(lon, lat);
+      }
+    );
 
+  // Init from a Geotiff filename
+  m.def("read_geo_camera", 
+    [](std::string filename) 
+    { 
+      vpgl_geo_camera* cam = new vpgl_geo_camera;
+      vil_image_resource_sptr img = vil_load_image_resource(filename.c_str());
+      vpgl_geo_camera::init_geo_camera(img, cam);
+      return cam;
+    },
+    "A function to read a geo camera from a geotiff header."
+  );
+
+  // Rational Camera Definitions
+  py::class_<vpgl_rational_camera<double> >(m, "rational_camera")
+    .def(py::init<>())
+    .def("__str__", stream2str<vpgl_rational_camera<double> >)
+    .def("project",
+      [](vpgl_rational_camera<double> &R, const double x, const double y, const double z)
+      {
+        vgl_point_3d<double> world_point(x,y,z);                                 
+        vgl_point_2d<double> img_point = R.project(world_point);
+        return std::make_tuple(img_point.x(), img_point.y());
+      }
+    );
+
+  vpgl_rational_camera<double>* (*read_rational_camera_from_file)(std::string) = &(read_rational_camera<double>);
+  m.def("read_rational_camera", read_rational_camera_from_file, "A function to read a rational camera from a file");
+
+  // Local Rational Camera Definitions
+  py::class_<vpgl_local_rational_camera<double> >(m, "local_rational_camera")
+    .def(py::init<>())
+    .def("__str__", stream2str<vpgl_local_rational_camera<double> >)
+    .def("project",
+      [](vpgl_local_rational_camera<double> &R, const double x, const double y, const double z)
+      {
+        vgl_point_3d<double> world_point(x,y,z);                                 
+        vgl_point_2d<double> img_point = R.project(world_point);
+        return std::make_tuple(img_point.x(), img_point.y());
+      }
+    );
+
+  vpgl_local_rational_camera<double>* (*read_local_rational_camera_from_file)(std::string) = &(read_local_rational_camera<double>);
+  m.def("read_local_rational_camera", read_local_rational_camera_from_file, "A function to read a local rational camera from a file");
+
+  // Compute Affine Camera from Local Rational Camera
+  m.def("compute_affine_from_local_rational", 
+    [](vpgl_local_rational_camera<double> &camera, 
+      double min_x, double min_y, double min_z, 
+      double max_x, double max_y, double max_z, unsigned n_points) 
+    {
+      if (n_points < 10)
+        n_points = 10;   // make it minimum 10 points
+
+      double width = max_x - min_x;
+      double depth = max_y - min_y;
+      double height = max_z - min_z;
+
+      std::cout << " Using: " << n_points << " to find the affine camera!\n";
+      std::cout << " w: " << width << " d: " << depth << " h: " << height << '\n';
+      std::vector< vgl_point_2d<double> > image_pts;
+      std::vector< vgl_point_3d<double> > world_pts;
+
+      vnl_random rng;
+      for (unsigned i = 0; i < n_points; i++) {
+        vgl_point_3d<float> corner_world;
+        double x = rng.drand64()*width + min_x;  // sample in local coords
+        double y = rng.drand64()*depth + min_y;
+        double z = rng.drand64()*height + min_z;
+        world_pts.push_back(vgl_point_3d<double>(x,y,z));
+        double u, v;
+        camera.project(x,y,z,u,v);  // local rational camera has an lvcs, so it handles 
+                                     // local coord to global to image point projection internally
+        image_pts.push_back(vgl_point_2d<double>(u,v));
+      }
+
+      vpgl_affine_camera<double>* out_camera = vpgl_affine_rectification::compute_affine_cam(image_pts, world_pts);
+      return out_camera;
+    }
+  );
 
   // =====LOCAL VERTICAL COORDINATE SYSTEM (LVCS)=====
   py::class_<vpgl_lvcs> lvcs(m, "lvcs");
